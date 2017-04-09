0d8ac6ba6a3955e0cd3c5102034079c53ee5b444
hobojoe1848 <sequeira.julian@gmail.com>
2017-04-09 20:14:57 +1000

    add day 011 - generic gmail emailing script

---
+++ 011/generic_emailer.py
@@ -0,0 +1,45 @@
+#!python3
+#emailer.py is a simple script for sending emails using smtplib
+#The idea is to assign a web-scraped file to the DATA_FILE constant.
+#The data in the file is then read in and sent as the body of the email.
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+DATA_FILE = 'scraped_data_file'
+from_addr = 'your_email@gmail.com'
+to_addr = ['some_recipient@gmail.com']
+msg = MIMEMultipart()
+msg['From'] = from_addr
+msg['To'] = ", ".join(to_addr)
+msg['Subject'] = 'My Subject Line'
+
+with open(DATA_FILE) as f:
+    body = f.read()
+#file_data = open(DATA_FILE)
+#body = file_data.read()
+#file_data.close()
+
+msg.attach(MIMEText(body, 'plain'))
+
+smtp_server = smtplib.SMTP('smtp.gmail.com', 587) #Specify Gmail Mail server
+
+smtp_server.ehlo() #Send mandatory 'hello' message to SMTP server
+
+smtp_server.starttls() #Start TLS Encryption as we're not using SSL.
+
+#Login to gmail: Account | Password
+smtp_server.login(' your_email@gmail.com ', ' GMAIL APPLICATION ID ')
+
+text = msg.as_string()
+
+#Compile email list: From, To, Email body
+smtp_server.sendmail(from_addr, to_addr, text)
+
+#Close connection to SMTP server
+smtp_server.quit()
+
+#Test Message to verify all passes
+print('Email sent successfully')
