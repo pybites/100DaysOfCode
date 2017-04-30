1db047a7379e6d978be7769caae96ee3fa357d71
hobojoe1848 <sequeira.julian@gmail.com>
2017-04-30 10:42:34 +1000

    day 031 - added os path line to make code os independent

--- 031/mv_files.py
+++ 031/mv_files.py
@@ -1,7 +1,4 @@
 #!python3
-#TODO: could potentially make this OS generic by adding code to detect OS type
-#then have it use the compatible folder syntax type for that OS. - ie, \ for Win
-# or / for Unix.
 
 import os
 import shutil
@@ -11,10 +8,10 @@ import shutil
 #Alternatively, (and preferably) you'd specify the full path of each dir.
 
 fromdir = "testdir1"
-todir = "testdir2\\"
+todir = "testdir2"
 
 files = os.listdir(fromdir)
 
 for i in files:
-	shutil.move(fromdir + "\\" + i, todir) #Adjust the \ or / based on OS type.
-	print("Moved %s" % i)
+	shutil.move(os.path.join(fromdir, i), todir)
+	print("Moved {} from {} to {}".format(i, fromdir, todir))
