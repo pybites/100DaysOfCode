1d36835e56a8edba4e00498c75e2f1fa1493cd4e
bbelderbos <bobbelderbos@gmail.com>
2017-05-26 23:44:29 +0200

    with dunder methods

--- 058/karma.py
+++ 058/karma.py
@@ -1,4 +1,5 @@
 roles = dict(employee=1, manager=2, director=3, svp=4, ceo=5)
+max_points = 3
 
 class Member:
     count = 0  # class variable
@@ -7,13 +8,29 @@ class Member:
         self.name = name
         self.role_id = role_id
         self.karma = karma
+        Member.count += 1
 
-    def add(self, points):
-        self.karma += points
+    '''
+    # these are cool but pretty obscure code like ---m and ++m
+    def __pos__(self):
+        self.karma += 1
 
-    def subtract(self, points):
-        self.karma -= points
+    def __neg__(self):
+        self.karma -= 1
+    '''
 
+    def _validation(self, points):
+        if not isinstance(points, int) or abs(points) > max_points:
+            raise ValueError('need int points of <= {}'.format(max_points))
+
+    def __lshift__(self, points):
+        self._validation(points)
+        self.karma += points 
+
+    def __rshift__(self, points):
+        self._validation(points)
+        self.karma -= points 
+        
     def __str__(self):
         return '{} (role_id: {}) has {} karma'.format(
             self.name, self.role_id, self.karma)
@@ -21,8 +38,26 @@ class Member:
 
 if __name__ == '__main__':
     m = Member('bob', roles.get('employee'), 0)
+    m << 3
+    try:
+        m << 4
+    except:
+        print('exception = good, should not give more than 3 points')
+    m >> 2
+    try:
+        m >> 5
+    except:
+        print('exception = good, should not subtract more than 3 points')
+    m >> 1
+    m << 3
+    assert m.karma == 3
     print(m)
-    m.add(2)
-    print(m)
-    m.subtract(3)
-    print(m)
+
+    mm = Member('tim', roles.get('manager'), 5)
+    mm << 2
+    mm << 3
+    assert mm.karma == 10
+    print(mm)
+
+    print()
+    print('The community now has {} active members'.format(Member.count))
