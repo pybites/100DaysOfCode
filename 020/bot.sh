5085fe8531c61d6dd5a88c8e44cb5dae0b49a4f6
bbelderbos <bobbelderbos@gmail.com>
2017-05-31 00:12:20 +0200

    redirect bot.sh error

--- 020/bot.sh
+++ 020/bot.sh
@@ -1,5 +1,5 @@
 cmd="$HOME/bin/python3/bin/python3.5 domain_mentions.py"
 until $cmd; do
-    echo "Slack bot crashed with exit code $?.  Respawning.." >&2
+    echo "Slack bot crashed with exit code $?.  Respawning.." >> bot.err
     sleep 1
 done
