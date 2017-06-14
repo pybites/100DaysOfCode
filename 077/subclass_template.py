5e2919df8aba9ba152c069c1dd3f1fbd45a13df7
bbelderbos <bobbelderbos@gmail.com>
2017-06-14 14:27:32 +0200

    flake8

--- 077/subclass_template.py
+++ 077/subclass_template.py
@@ -1,37 +1,38 @@
 #!python3
-#subclass_template.py is a usable, blank template for creating a Python Class with subclass.
+# subclass_template.py is a usable, blank template
+# for creating a Python Class with subclass.
+
 
 class Class1(object):
     def __init__(self, attribute1, attribute2, attribute3):
         self.attribute1 = attribute1
         self.attribute2 = attribute2
         self.attribute3 = attribute3
-    
-    #Class Function Examples:
-    
+
+    # Class Function Examples:
+
     def ClassFunction_1(self):
-        #<do something>
-        return something
-    
+        pass
+
     def ClassFunction_2(self):
         return self.attribute2
-    
+
     def ClassFunction_3(self):
         variable = int(self.attribute1 + self.attribute3)
         return variable
-        
-#Define an "empty" subclass off the primary class above
+
+
+# Define an "empty" subclass off the primary class above
 class SubClass1(Class1):
-    def __init__(self, 
-                attribute1,
-                attribute2,
-                attribute3,
-                sub_attr_1,
-                sub_attr_2):
+    def __init__(self,
+                 attribute1,
+                 attribute2,
+                 attribute3,
+                 sub_attr_1,
+                 sub_attr_2):
         super().__init__(attribute1, attribute2, attribute3)
         self.sub_attr_1 = sub_attr_1
         self.sub_attr_2 = sub_attr_2
-        
-    
+
     def SubClassFunction_1(self):
         return self.sub_attr_1
