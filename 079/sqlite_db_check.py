472fdab192f1baf5995a4bb033ef7fdbe9e257a2
hobojoe1848 <sequeira.julian@gmail.com>
2017-06-16 21:00:19 +1000

    Day 079 - sqlite create with exceptions script

---
+++ 079/sqlite_db_check.py
@@ -0,0 +1,16 @@
+#!python3
+#sqlite_db_check.py is a script to create a db and capture exceptions.
+#The script will create a specified db on the first run.
+#The exception is handy because on subsequent runs, it prints a message
+#that the DB exists. It'll also print any other actual errors. Useful!
+
+import sqlite
+import sys
+
+with sqlite3.connect("my_database.db") as connection:
+    c = connection.cursor()
+    try:
+        c.execute("CREATE TABLE table-name (column1 TEXT, column2 TEXT")
+    except Exception as e:
+        print("Could not create the database. DB either already exists or there was an error (see output below):")
+        print("Exception: {}".format(str(e)))
