5cf337db4506889292128aab4f40f3418d12e052
bbelderbos <bobbelderbos@gmail.com>
2017-06-16 13:08:09 +0200

    added optional caching to collect data for testing, also have output return string to make it testable

--- 080/whotweeted.py
+++ 080/whotweeted.py
@@ -3,15 +3,18 @@ Who am I talking to PyBites?!
 Fair question: https://twitter.com/anthonypjshaw/status/875275923930480641
 '''
 
+from functools import wraps
 import os
+import pickle
 import re
 import sys
 
 import tweepy
 
+DATA = 'data'
 KNOWN_PLACES = dict(AU='Julian', ES='Bob')
 PYBITES = 'pybites'
-
+COLLECT_TEST_DATA = False
 TWITTER_KEY = os.environ.get('PYB_100D_TW_KEY')
 TWITTER_SECRET = os.environ.get('PYB_100D_TW_SECRET')
 if not TWITTER_KEY or not TWITTER_SECRET:
@@ -22,12 +25,34 @@ auth = tweepy.OAuthHandler(TWITTER_KEY, TWITTER_SECRET)
 api = tweepy.API(auth)
 
 
+def cache(f):
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        tweetid = args[0]
+        cache = os.path.join(DATA, tweetid)
+        r = f(*args, **kwargs)
+        if COLLECT_TEST_DATA:
+            pickle.dump(r, open(cache, "wb"))
+        return r
+    return wrapped
+
+
+def load_cache(tweetid):
+    cache = os.path.join(DATA, tweetid)
+    return pickle.load(open(cache, "rb"))
+
+
+@cache
+def get_status(tweetid):
+    return api.get_status(tweetid)
+
+
 def get_country_code(tweetid):
     try:
-        tweet = api.get_status(tweetid)
-    except tweepy.error.TweepError:
-        raise ValueError('Not a valid tweetid')
-
+        tweet = get_status(tweetid)
+    # this could be various issues so print the exception string
+    except tweepy.error.TweepError as exc:
+        raise ValueError('Problem getting tweet:\n{}'.format(exc))
     if tweet.user.screen_name != PYBITES:
         raise ValueError('Not a {} tweet'.format(PYBITES))
 
@@ -39,16 +64,12 @@ def get_country_code(tweetid):
 
 def who_is_output(country):
     if not country:
-        print('Unable to identify country for tweet id')
-        return
+        return 'Unable to identify country code'
 
     if country not in KNOWN_PLACES:
-        print('Tweet was tweeted from {}'.format(country), end=', ')
-        print('is PyBites traveling or fooling the system?!')
-        return
+        return 'WTF?! Tweet was tweeted from {}'.format(country)
 
-    print('{} tweeted out {}'.format(KNOWN_PLACES[country],
-                                     tweetid))
+    return '{} tweeted it out'.format(KNOWN_PLACES[country])
 
 
 if __name__ == '__main__':
@@ -61,7 +82,8 @@ if __name__ == '__main__':
         country_code = get_country_code(tweetid)
     except ValueError as exc:
         print(exc)
-    except AttributeError:
+    except AttributeError as exc:
         print('Location not set on tweet')
     else:
-        who_is_output(country_code)
+        output = who_is_output(country_code)
+        print(output)
